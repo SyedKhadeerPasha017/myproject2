d1e6f55d4220cbf02ad5b033a2ae1123ada2d1db
SyedKhadeerPasha017 <Khadeerpasha017@gmail.com>
2024-02-15 12:20:14 +0530

    version2 completed

---
+++ good.java
@@ -0,0 +1,9 @@
+class good
+{
+public static void main(String args[])
+{
+System.out.println("welcome");
+System.out.println("home");
+
+}
+}
---
+++ sir.java
@@ -0,0 +1,9 @@
+class sir
+{
+public static void main(String args[])
+{
+System.out.println("welcome");
+System.out.println("home");
+
+}
+}
